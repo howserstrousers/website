dc38a97bafe76210e605ea3e86fed20b80a3e510
howserstrousers <fritzcharleshowser@gmail.com>
2016-05-05 15:49:49 -0400

    up

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,25 @@ $(document).ready(function(){
         $(".about").toggle();
     });
 
-    
+    $(".indrop").click(function(){
+        $(".ipics").toggle();
+    });
+
+    $(".outdrop").click(function(){
+        $(".opics").toggle();
+    });
+
+    $(".rdrop").click(function(){
+        $(".rpics").toggle();
+    });
+
+    $(".gifdrop").click(function(){
+        $(".ipics, .opics, .rpics").toggle();
+    });
+
+
+
+
 
 });
 
